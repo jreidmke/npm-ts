0ceb589971d233bd7c1cc415d464be45d6d8e99b
James Reid <jreidmke@gmail.com>
2022-12-06 15:53:14 -0600

    clean

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as Tooltip from "@radix-ui/react-tooltip";
 import { PlusIcon } from "@radix-ui/react-icons";
-import "./Tooltip.css";
+import "./Tooltip.scss";
 
 const CustomTooltip = () => {
     return (
