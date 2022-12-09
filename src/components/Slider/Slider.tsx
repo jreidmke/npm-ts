38224e526d9e215b257aaa5c282b184e1870221d
James Reid <jreidmke@gmail.com>
2022-12-09 10:44:17 -0600

    new slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,22 +1,60 @@
+import * as SliderPrimitive from "@radix-ui/react-slider";
+import cx from "classnames";
 import React from "react";
-import * as Slider from "@radix-ui/react-slider";
-import "./Slider.scss";
 
-const CustomSlider = () => (
-    <form>
-        <Slider.Root
-            className="SliderRoot"
-            defaultValue={[50]}
-            max={100}
-            step={1}
-            aria-label="Volume"
+export type SliderProps = {
+    defaultValue: number[] | undefined;
+    max: number;
+    step: number;
+    uxWidth?: string; //pass in as tailwind w-*
+    trackColor?: string;
+    thumbColor?: string;
+    thumbOutlineColor?: string;
+    additionalTrackProps?: string;
+    additionalThumbProps?: string;
+};
+
+const Slider = ({
+    defaultValue,
+    max,
+    step,
+    uxWidth,
+    trackColor,
+    thumbColor,
+    additionalTrackProps,
+    additionalThumbProps,
+}: SliderProps) => {
+    return (
+        <SliderPrimitive.Root
+            defaultValue={defaultValue}
+            max={max}
+            step={step}
+            aria-label="value"
+            className={`relative flex h-5 touch-none items-center ${
+                uxWidth ? uxWidth : "w-64"
+            }`}
         >
-            <Slider.Track className="SliderTrack">
-                <Slider.Range className="SliderRange" />
-            </Slider.Track>
-            <Slider.Thumb className="SliderThumb" />
-        </Slider.Root>
-    </form>
-);
+            <SliderPrimitive.Track
+                className={`relative h-1 w-full grow rounded-full ${
+                    trackColor ? trackColor : "bg-black"
+                } ${additionalTrackProps}`}
+            >
+                <SliderPrimitive.Range
+                    className={`absolute h-full rounded-full bg-black ${
+                        trackColor ? trackColor : "bg-black"
+                    } ${additionalTrackProps}`}
+                />
+            </SliderPrimitive.Track>
+            <SliderPrimitive.Thumb
+                className={cx(
+                    `block h-5 w-5 rounded-full ${
+                        thumbColor ? thumbColor : "bg-black"
+                    }`,
+                    additionalThumbProps
+                )}
+            />
+        </SliderPrimitive.Root>
+    );
+};
 
-export default CustomSlider;
+export default Slider;
--- src/stories/Slider.stories.tsx
+++ src/stories/Slider.stories.tsx
@@ -8,7 +8,23 @@ export default {
     component: Slider,
 } as ComponentMeta<typeof Slider>;
 
-const Template: ComponentStory<typeof Slider> = (args) => <Slider />;
+const Template: ComponentStory<typeof Slider> = (args) => <Slider {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+    defaultValue: [50],
+    max: 100,
+    step: 1,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+    defaultValue: [25],
+    max: 200,
+    step: 5,
+    uxWidth: "w-128",
+    trackColor: "bg-red-500",
+    thumbColor: "bg-blue-900",
+    additionalThumbProps:
+        "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75",
+};
